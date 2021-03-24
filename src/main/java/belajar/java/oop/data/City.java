0dc30fa2399bdae3ce95da5afc9d223ac7a27462
denanda prayoga <denanda79@gmail.com>
2021-03-24 21:25:58 +0700

    abstract class

---
+++ src/main/java/belajar/java/oop/data/City.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar.java.oop.data;
+
+/**
+ *
+ * @author zero
+ */
+public class City extends Location {
+    public static void main(String[] args) {
+        // Location location = new Location();// ERROR karena abstract class
+        City city = new City();
+        city.name = "Citayam";
+        System.out.println(city.name);
+    }
+}
